d9716d10e8e1b65661871236c6f320404e070655
Varun Thomas <varunthomas.mec@gmail.com>
2022-10-07 12:20:31 +0530

    Dungeon part 1

---
+++ Assets/Scripts/DungeonGenerator.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonGenerator : MonoBehaviour
+{
+	
+	public static DungeonGenerator instance;
+	void Awake()
+	{
+		
+		if (instance == null)
+        {
+            
+        }
+		
+	}
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
